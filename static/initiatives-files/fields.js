6b5461fca7ad26526f7f5e28cf2f14f0eadca29d
Alexander Thiel <me@alexthiel.de>
2020-02-18 18:18:42 -0500

    submodule cleanup v2

---
+++ static/initiatives-files/fields.js
@@ -0,0 +1,49 @@
+exports.fields = {
+
+    basics: {
+        title: 'Basics',
+        children: {
+            meta: {title: 'Initiative'},
+            description: {title: 'Description'},
+            established: {title: 'Established'},
+            kpis: {title: 'KPIs'},
+            actions: {title: 'Actions'},
+        },
+    },
+    internationality: {
+        title: 'Internationality',
+        children: {
+            activeCountries: {title: 'Active Countries'},
+        },
+    },
+    people: {
+        title: 'People',
+        children: {
+            employees: {title: 'Employees'},
+            participation: {title: 'Participation'},
+            // members, participants
+        },
+    },
+    resources: {
+        title: 'Resources',
+        children: {
+            yearlyIncome: {title: 'Yearly Income'},
+            // revenue, foerdermittel, spenden, sonstiges
+        },
+    },
+    reach: {
+        title: 'Reach',
+        children: {
+            socialMedia: {title: 'Social Media'},
+            pressArticles: {title: 'Press Articles'},
+        },
+    },
+    // legislativeParticipation: {
+    //     title: 'Legislative Participation',
+    //     children: {
+    //         lobbyistsRegistered: {title: 'Registered Lobbyists'},
+    //         legislativeParticipation: {title: 'Participation'},
+    //         billsDrafted, Bills Enacted
+        // },
+    // },
+};
---
+++ static/initiatives-files/script.js
@@ -0,0 +1,53 @@
+function renderInitiatives(tableId, {data, fields}) {
+    
+    document.querySelector(`#${tableId} tbody`)
+        .insertAdjacentHTML(
+            'beforeend',
+            Object.keys(fields)
+                .reduce(
+                    (acc, group) => [
+                        [
+                            ...acc[0],
+                            `<th class="${group}" colspan="${Object.keys(fields[group].children).length}">${fields[group].title}</th>`
+                        ],
+                        [
+                            ...acc[1],
+                            ...Object.keys(fields[group].children).map(field => `<th class="${group}">${fields[group].children[field].title}</th>`),
+                        ],
+                    ],
+                    [[], []]
+                )
+                .map(thSet => `<tr>${thSet.join('\n')}</tr>`)
+                .join('\n'));
+    const flatFields = Object.keys(fields)
+        .reduce(
+            (acc, group) => ({
+                ...acc,
+                ...fields[group].children
+            }),
+            {});
+    const md = window.markdownit();
+    const render = (value, initiative) =>
+        `<td>${
+            value === undefined
+                ? '-'
+                : value.name && value.link
+                ? `<p><a href="${value.link}">${value.name}</a></p>`
+                : value.name
+                    ? value.name
+                    : `<span class="updated">${value.updatedBy} (<a href="https://github.com/crisisfighters/initiatives/pulls?q=is%3Apr+label%3A${initiative.meta.label}">${value.updatedAt}</a>):</span>${
+                        md.render(value.content)
+                        }`
+            }</td>`
+    ;
+    document.querySelector(`#${tableId} tbody`)
+        .insertAdjacentHTML(
+            'beforeend',
+            data.map(initiative => `<tr>${
+                Object.keys(flatFields)
+                    .map(field => render(initiative[field], initiative))
+                    .join('')
+                }</tr>`)
+                .join('\n')
+        );
+};
