0d3de000302156f59194789629868efdc6b6d1cc
Alexander Thiel <me@alexthiel.de>
2020-02-19 17:41:27 -0500

    link target now set to blank by default

--- static/initiatives-files/recruiter.js
+++ static/initiatives-files/recruiter.js
@@ -86,6 +86,27 @@ function renderLocationSelector(params, resultDescriptor) {
       });
 }
 
+function setLinkTargetToBlank(md) {
+    // Thanks to https://github.com/markdown-it/markdown-it/blob/bda94b0521f206a02427ec58cb9a848d9c993ccb/docs/architecture.md
+    
+    const defaultRender = md.renderer.rules.link_open || function(tokens, idx, options, env, self) {
+        return self.renderToken(tokens, idx, options);
+    };
+
+    md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+        // If you are sure other plugins can't add `target` - drop check below
+        const aIndex = tokens[idx].attrIndex('target');
+    
+        if (aIndex < 0) {
+            tokens[idx].attrPush(['target', '_blank']); // add new attribute
+        } else {
+            tokens[idx].attrs[aIndex][1] = '_blank';    // replace value of existing attr
+        }
+        // pass token to default renderer.
+        return defaultRender(tokens, idx, options, env, self);
+    };
+
+}
 function renderResultScreen(params, {result: elements, location}) {
 
     console.log(location);
@@ -93,7 +114,8 @@ function renderResultScreen(params, {result: elements, location}) {
     const isPresentInCity = () => false;
 
     const md = window.markdownit();
-
+    setLinkTargetToBlank(md);
+    
     const renderResults = elements => `
         <h4>The data you entered
         (<a href="https://services342876.typeform.com/to/H8DLJt">start over</a>)
