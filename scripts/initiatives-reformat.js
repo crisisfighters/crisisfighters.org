b46ce0b31de34cb67bf00fe88fc5448b7bbe210a
Alexander Thiel <me@alexthiel.de>
2020-02-20 12:44:37 -0500

    adds scripts to pull tags from sheet

---
+++ scripts/initiatives-reformat.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+const { writeFileSync, readFileSync } = require('fs');
+const { join } = require('path');
+
+const path = join(__dirname, '..','static', 'initiatives-files', 'initiatives.js');
+
+writeFileSync(
+    path,
+    'exports.initiatives = ' + JSON.stringify(
+        JSON.parse(
+            readFileSync(path)
+            .toString()
+            .replace(/^exports\.initiatives = /, '')
+            .replace(/;$/, '')
+        ),
+        null,
+        2
+    ) + ';'
+);
