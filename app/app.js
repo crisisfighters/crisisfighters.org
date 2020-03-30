2fe599fcd79ec0127fb9e2f32e6b4814a03ce1b1
Alexander Thiel <me@alexthiel.de>
2020-03-30 17:26:51 +0200

    supports browsers with no matching languages

--- app/app.js
+++ app/app.js
@@ -12,9 +12,24 @@ const pathFromPermalink = p => p.replace(/^https?:\/\/[^\/]+(\/.*)$/, '$1');
 const cookieContent = Cookies.get().language;
 const hasCookie = typeof cookieContent === "string" && cookieContent.length > 0;
 
-if(!navigator.languages.includes(cfLanguages.current) && !hasCookie) {
+let navigatorLanguages = navigator.languages || [navigator.language];
+const availableLanguages = [
+    window.cfLanguages.current,
+    window.cfLanguages.available.map(l => l.lang),
+];
+if(!navigatorLanguages.some(l => availableLanguages.includes(l))) {
+    // UA likes no language we have in store.
+    // Let's pretend the UA likes our fallback language.
+    navigatorLanguages = [window.cfLanguages.fallback];
+}
+
+if(!navigatorLanguages.includes(cfLanguages.current) && !hasCookie) {
     console.log('Switching language')
-    const newLanguage = window.cfLanguages.available.filter(l => navigator.languages.includes(l.lang))[0] || 0;
+    let newLanguage = window.cfLanguages.available.filter(l => navigatorLanguages.includes(l.lang))[0];
+    if(!newLanguage) {
+       newLanguage = window.cfLanguages.available.filter(l => l.lang === window.cfLanguages.fallback)[0];
+    }
+    console.log(newLanguage);
     const href = pathFromPermalink(newLanguage.permalink);
     document.location.href = href;
 }
