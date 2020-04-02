fd0d6002c70ff433c2de79d7619d5dd2c9a1a790
Alexander Thiel <me@alexthiel.de>
2020-04-02 17:02:38 +0200

    faas test 3

--- dist-functions/test.js
+++ dist-functions/test.js
@@ -1,4 +1,4 @@
-exports.test = function(event, context, callback) {
+exports.handler = function(event, context, callback) {
     callback(null, {
     statusCode: 200,
     body: "Hello, World\n" + JSON.stringify(event)
